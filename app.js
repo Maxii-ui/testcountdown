5b008ebc1bf92289653ce1ca662c8099c901893a
James Mortemore <james.mortemore@clock.co.uk>
2014-03-20 10:11:51 +0000

    Updated to use hud-widget

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
+var Widget = new require('hud-widget')
+  , widget = new Widget()
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , app = express()
-
-// all environments
-app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-app.use(express.favicon())
-app.use(express.logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.methodOverride())
-app.use(app.router)
-app.use(express.static(path.join(__dirname, 'public')))
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
-}
-
-app.get('/', function (req, res) {
+widget.get('/', function (req, res) {
   var events = JSON.parse(process.env.EVENTS)
     , lastEventDate = new Date()
     , newEventDate = new Date()
@@ -50,8 +26,4 @@ app.get('/', function (req, res) {
   }
 
   res.render('index', { nextEvent: nextEvent, lastEventDate: lastEventDate.toString() })
-})
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'))
 })
